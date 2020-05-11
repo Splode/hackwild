e6f8195e9bc1cad6d9e0af5ce79aa156507e5b3f
Christopher Murphy <flyweight@pm.me>
2020-05-11 12:51:20 -0600

    chore: upgrade jekyll-vue

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,13 +1,14 @@
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
 const path = require('path')
 const webpack = require('webpack') // eslint-disable-line
-const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist', // needed to appropriately reference build.js in html
+    publicPath: '/dist',
     filename: '[name].[chunkhash].js'
   },
   optimization: {
@@ -22,15 +23,13 @@ module.exports = {
       }
     }
   },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      filename: './../_includes/scripts.html',
-      template: './_includes/_scripts.html' // _scripts is master due to redundant script tags
-    })
-  ],
+  mode: 'development',
   module: {
     rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
       {
         test: /\.css$/,
         use: ['vue-style-loader', 'css-loader']
@@ -40,29 +39,28 @@ module.exports = {
         use: ['vue-style-loader', 'css-loader', 'sass-loader']
       },
       {
-        test: /\.sass$/,
-        use: ['vue-style-loader', 'css-loader', 'sass-loader?indentedSyntax']
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-            scss: ['vue-style-loader', 'css-loader', 'sass-loader']
-          }
-        }
-      },
-      {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: file => /node_modules/.test(file) && !/\.vue\.js/.test(file)
       }
     ]
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: './../_includes/scripts.html',
+      template: './_includes/_scripts.html'
+    }),
+    new VueLoaderPlugin()
+  ],
   resolve: {
     alias: {
       vue$: 'vue/dist/vue.esm.js'
     },
     extensions: ['*', '.js', '.vue', '.json']
   }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.mode = 'production'
 }
